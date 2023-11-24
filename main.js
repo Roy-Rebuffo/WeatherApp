00eb793fffa3846710d338cffedb5757743183d6
Roy Rebuffo <roy.rebuffo@bootcamp-upgrade.com>
2023-11-24 15:44:39 +0100

    agrego busqueda por enter

--- main.js
+++ main.js
@@ -13,10 +13,8 @@ async function checkWeather(city) {
   if (response.status == 404) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
-    document.querySelector(".sun").style.display = "none";
   } else {
     let data = await response.json();
-
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML =
       Math.round(data.main.temp) + "°C";
@@ -25,18 +23,15 @@ async function checkWeather(city) {
 
     if (data.weather[0].main == "Clouds") {
       weatherIcon$$.src = "assets/clouds.png";
-      card$$.src = ""
     } else if (data.weather[0].main == "Clear") {
         weatherIcon$$.src = "assets/clear.png";
     } else if (data.weather[0].main == "Rain") {
       weatherIcon$$.src = "assets/rain.png";
-      
     } else if (data.weather[0].main == "Drizzle") {
       weatherIcon$$.src = "assets/drizzle.png";
       
     } else if (data.weather[0].main == "Mist") {
       weatherIcon$$.src = "assets/mist.png";
-      
     }
     document.querySelector(".weather").style.display = "block";
     document.querySelector(".error").style.display = "none";
@@ -46,5 +41,6 @@ async function checkWeather(city) {
 searchBtn$$.addEventListener("click", () => {
   checkWeather(searchBox$$.value);
 });
+
 
 checkWeather();
