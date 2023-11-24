ae2d4ad07a3017edd4fe257c7562b5b60c84857f
Roy Rebuffo <roy.rebuffo@bootcamp-upgrade.com>
2023-11-24 16:47:49 +0100

    backgrounds personalizados para cada tiempo

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const baseUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const baseUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const apiKey = "588f9a07616dc0b9058142ad6d5930d3";
 const searchBox$$ = document.querySelector(".search input");
 const searchBtn$$ = document.querySelector(".search button");
@@ -23,15 +22,19 @@ async function checkWeather(city) {
 
     if (data.weather[0].main == "Clouds") {
       weatherIcon$$.src = "assets/clouds.png";
+      card$$.classList.add("clouds");
     } else if (data.weather[0].main == "Clear") {
         weatherIcon$$.src = "assets/clear.png";
+        card$$.classList.add("clear");
     } else if (data.weather[0].main == "Rain") {
       weatherIcon$$.src = "assets/rain.png";
+      card$$.classList.add("rain");
     } else if (data.weather[0].main == "Drizzle") {
       weatherIcon$$.src = "assets/drizzle.png";
-      
+      card$$.classList.add("drizzle");
     } else if (data.weather[0].main == "Mist") {
       weatherIcon$$.src = "assets/mist.png";
+      card$$.classList.add("mist");
     }
     document.querySelector(".weather").style.display = "block";
     document.querySelector(".error").style.display = "none";
